1aa0798a4836b90ff7a3d5fb7346c74009e2a296
ayman <aymanshaik1015@gmail.com>
2025-09-22 11:48:22 +0530

    ci: angular

--- sdk/qrcode-angular/src/lib/components/self-qrcode/self-qrcode.component.ts
+++ sdk/qrcode-angular/src/lib/components/self-qrcode/self-qrcode.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit, signal, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { v4 as uuidv4 } from 'uuid';
 
