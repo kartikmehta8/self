9033a1e0e2dd94ce4d56f986ea8bf91174d0ecf1
Kartik Mehta <kartikmehta202@gmail.com>
2025-12-27 21:54:51 +0530

    Remove old LED implementation and occurences

---
+++ sdk/qrcode/animations/index.ts
@@ -0,0 +1,6 @@
+import statusConnecting from '../animations/status-connecting.json' with { type: 'json' };
+import statusError from '../animations/status-error.json' with { type: 'json' };
+import statusFailed from '../animations/status-failed.json' with { type: 'json' };
+import statusSuccess from '../animations/status-success.json' with { type: 'json' };
+
+export { statusConnecting, statusError, statusFailed, statusSuccess };
